4a0d5d22e132bf9feb5a8eb0a4a5fabf9f64af0b
WilliamPhilippe <wplb@ic.ufal.br>
2019-01-08 22:51:52 -0300

    Create 1143747.cpp

---
+++ 1143747/1143747.cpp
@@ -0,0 +1,25 @@
+static int getseword(struct protstream *prot, char *buf, int maxlen){
+
+    int c = EOF;
+    int quoted = 0;
+
+    c = prot_getc(prot);
+    if (c == '"') quoted = 1;
+    else prot_ungetc(c, prot);
+
+    while( maxlen > 1){
+        c = prot_getc(prot);
+        if(c == EOF) break;
+        if(quoted && c == '"') break;
+        if(!quoted && ( c == ' ' || c == ')' ) ) break;
+
+        *buf++ = c;
+        maxlen--;
+    }
+
+    *buf = '\0';
+
+    if (quoted && c != EOF) c = prot_getc(prot);
+
+    return c;
+}
