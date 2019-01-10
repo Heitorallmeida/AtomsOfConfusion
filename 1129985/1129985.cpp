536adfdfe9fb627f1f59ec11c03fd54233f29bc8
WilliamPhilippe <wplb@ic.ufal.br>
2019-01-10 10:58:59 -0300

    Update 1129985.cpp

--- 1129985/1129985.cpp
+++ 1129985/1129985.cpp
@@ -13,25 +13,23 @@ char_u * vim_strchr(char_u * name, char x){
 	else return NULL;
 }
 
-static char_u * make_pull_name(char_u * name){
 
-    char_u *pname, *p;
+    static char_u *
+make_pull_name(char_u * name)
+{
+    char_u  *pname;
+    char_u  *p;
 
     pname = vim_strnsave(name, strlen(name) + strlen("-pullright"));
-    if(pname != NULL){
-        strcat( (char *) pname, "-pullright" );
-
-        p = vim_strchr(name, '.');
-        while(p != NULL){
-
-            *p = '_';
-
-            p = vim_strchr(name, '.');
-        }
+    if (pname != NULL)
+    {
+	strcat((char *)pname, "-pullright");
+	while ((p = vim_strchr(pname, '.')) != NULL)
+	    *p = '_';
     }
-
     return pname;
 }
+
 
 int main(){
 
